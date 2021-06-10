267baae722f7454004220dbcf4a0b4a54ff8994c
DongHwan Kim <hwan.kim@navercorp.com>
2021-06-11 07:10:18 +0900

    fix filepath

--- hevc_test.py
+++ hevc_test.py
@@ -57,8 +57,8 @@ def parse_ffmpeg(p):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    input_path = '../../original/4k'
-    output_path = '../../result/4k'
+    input_path = '../original/4k'
+    output_path = '../result/4k'
     presets = ['ultrafast',
                'superfast',
                'veryfast',
